16f6d6fe5a4bbd219f77287bb98ae81276824e6a
Piyush Khandelwal <piyushkhandelwal993@gmail.com>
2022-05-01 20:28:56 +0530

    Binary search recursion

--- binary_search.py
+++ binary_search.py
@@ -1,12 +1,14 @@
-def binary(arr,x):
-	start = 0
-	end = len(arr)-1
-	while(start <= end):
-		mid = (start + end)//2
-		if arr[mid] == x:
-			return mid
-		elif arr[mid] > x:
-			end = mid - 1
-		else:
-			start = mid + 1
-	return -1
+def binary(arr,x,start ,end):
+	
+	if start > end:
+		return -1
+
+	mid = (start + end)//2
+	if(arr[mid]==x):
+		return mid
+	elif(arr[mid]>x):
+		return binary(arr,x,start,mid-1)
+	else:
+		return binary(arr,x,mid+1,end)
+
+	
